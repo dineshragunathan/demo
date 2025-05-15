c768140fd0549759abdd531566605ec5eada2077
Dinesh Ragunathan <dinesh@MacBookAir.mshome.net>
2025-05-15 12:19:08 +0530

    frontend containg dashboard

---
+++ src/app/components/header.js
@@ -0,0 +1,7 @@
+export default function header() {
+    return (
+        <div className="flex flex-row bg-black h-[7vh] items-center p-3 text-3xl">
+            CITadel
+        </div>
+    );
+}
---
+++ src/app/components/sideBar.js
@@ -0,0 +1,9 @@
+export default function sideBar() {
+    return (
+        <div className="bg-gray-700 h-screen w-[15vw] text-gray-200 flex px-2 justify-center p-5">
+            <ul>
+                <li className="hover:underline hover:cursor-pointer text-xl">Dashboard</li>
+            </ul>
+        </div>
+    );
+}
